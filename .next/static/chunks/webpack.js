376d40ebd89120dff5a8e669aedd953791061928
Christian Lung <christianlung@wifi-131-179-2-240.host.ucla.edu>
2023-04-22 20:41:16 -0700

    CSS for home page with Grid implementation

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "b28d2b75d8ee63e5"; }
+/******/ 		__webpack_require__.h = function() { return "edab047f16e7aa23"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
