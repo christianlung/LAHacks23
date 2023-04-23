d40f662d22526756e0dd9f913511c45bc2cf28e6
Ajay Bhatia <ajbhatia@ucsc.edu>
2023-04-22 20:05:33 -0700

    updated video display

--- src/pages/home.js
+++ src/pages/home.js
@@ -2,6 +2,7 @@ import { useEffect, useRef, useState} from 'react';
 import { addDoc, getDocs, collection } from "@firebase/firestore"
 import { firestore } from "../firebase_setup/firebase.js"
 import { CircularProgress, Grid} from '@mui/material';
+import Video from '../components/Video.js';
 import Router from "next/router"
 
 function App() {
@@ -25,55 +26,17 @@ function App() {
     return <CircularProgress />
   } else {
     return (
-      <div className="App">
-      <h1>Video List</h1>
-      <button onClick={fetchVideos}>Refresh Videos</button>
-      <div>
-        {videos.map((video) => (
-          <div key={video.id} style={{margin: "1rem"}}>
-            <h3>{video.title}</h3>
-            <iframe
-              width="560"
-              height="315"
-              src={`${video.url}?autoplay=1&mute=1`}
-              title={video.title}
-              frameborder="0"
-              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-              allowFullScreen
-            ></iframe>
-            </div>
-        ))}
-      </div>
-    </div>
+      <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
+        <Grid container item justifyContent="space-evenly" alignContent="center" wrap="wrap">
+          {videos.map((video) => (
+            <Grid item key={video.id}>
+              <Video url={video.url} sx={{ width: "50%", height: "70%" }}/>
+            </Grid>
+          ))}
+        </Grid>
+      </Grid>
     )
   }
- 
-  // return (
-  //   <div className="App">
-  //     <form onSubmit={submithandler}>
-  //       <input type= "text" ref={dataRef} />
-  //       <button type = "submit">Save</button>
-  //       <button type = "button" onClick={logData}>Fetch</button>
-  //     </form>
-  //   </div>
-  // // );
-  // return (
-  //   <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
-  //     <Grid item xs={6}>
-  //       <Videos/>
-  //     </Grid>
-  //     <Grid item xs={6}>
-  //       <Videos/>
-  //     </Grid>
-  //     <Grid item xs={6}>
-  //       <Videos/>
-  //     </Grid>
-  //     <Grid item xs={6}>
-  //       <Videos/>
-  //     </Grid>
-  //   </Grid>
-  // );
-
 }
  
 export default App;
