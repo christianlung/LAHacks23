6f5fd2453451720205d42b4eb636c78dbe4ef062
Christian Lung <christianlung@Christians-MacBook-Air.local>
2023-04-22 21:52:44 -0700

    Updated home css

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,16 @@
 import { Typography, Grid, Button, TextField, InputAdornment } from "@mui/material"
 import MyLocationIcon from '@mui/icons-material/MyLocation';
 import { useState } from "react";
-
+import {useRouter} from "next/router";
 function Landing() {
-
+    const router = useRouter();
     const [val, setVal] = useState("");
-
     return (
         <Grid container direction="column" spacing={8} justifyContent="space-between" alignItems="center" backgroundColor="black"
         sx={{paddingTop: "10%", paddingBottom: "400px"}}> 
-            {/* <Grid item>
-                <Typography variant="h3" color="white">off the map</Typography>
-            </Grid> */}
+            <Grid item>
+                <Typography variant="h3" color="white">Input current location</Typography>
+            </Grid>
             <Grid item >
                 <TextField id="outlined-basic" variant="outlined" helperText="Location" value={val} onChange={(e) => setVal(e.target.value)}
                  InputProps={{
@@ -20,7 +19,7 @@ function Landing() {
                   }} />
             </Grid>
             <Grid item>
-                <Button variant="contained" color="primary">Go!</Button>
+                <Button onClick={() => router.push("/home")}variant="contained" color="primary">Go!</Button>
             </Grid>
 
         </Grid>
